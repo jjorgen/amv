89aef5c6bd53ebe736db889db3e644318e614596
amv <jjorgensen2311@gmail.com>
2016-08-14 08:10:14 -0400

    Initial commit

---
+++ src/test/java/org/nsu/dcis/AmvTest.java
@@ -0,0 +1,38 @@
+package org.nsu.dcis;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple Amv.
+ */
+public class AmvTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AmvTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AmvTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
