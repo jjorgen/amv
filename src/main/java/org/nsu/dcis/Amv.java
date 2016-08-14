75661f31b130cd6687326ae678123b755c2af241
amv <jjorgensen2311@gmail.com>
2016-08-14 12:02:30 -0400

    updated text to be printed to console

--- src/main/java/org/nsu/dcis/Amv.java
+++ src/main/java/org/nsu/dcis/Amv.java
@@ -4,6 +4,6 @@ public class Amv
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Aspect Mining and Visualization Tool" );
     }
 }
