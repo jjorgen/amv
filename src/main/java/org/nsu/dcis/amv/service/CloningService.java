6242731c74a7d635a8ef2c71e2488a913623b7f9
amv <jjorgensen2311@gmail.com>
2016-09-11 13:02:37 -0400

    Added clone scanner.

--- src/main/java/org/nsu/dcis/amv/service/CloningService.java
+++ src/main/java/org/nsu/dcis/amv/service/CloningService.java
@@ -13,7 +13,6 @@ public class CloningService {
     CloneScanner cloneScanner;
 
     public CloneResult findClones() {
-
         return new CloneResult();
     }
 }
--- src/main/java/org/nsu/dcis/amv/util/CloneScanner.java
+++ src/main/java/org/nsu/dcis/amv/util/CloneScanner.java
@@ -1,4 +1,11 @@
 package org.nsu.dcis.amv.util;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CloneScanner {
+
+    public CloneResult scan(String rootDirectory) {
+        return new CloneResult();
+    }
 }
